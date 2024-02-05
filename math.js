9869568e74b02a2b64394ebcf0bbd42e47a035eb
DevRahat <dev.rahatislam@gmail.com>
2024-02-05 09:55:33 +0600

    temp

---
+++ math.js
@@ -0,0 +1,14 @@
+
+
+// const min=Math.min(12,23,-3,45);
+
+// console.log(min);
+
+// const max=Math.max(12,34,56,74,34,97,23);
+
+// console.log(max);
+
+// console.log(Math.abs(-88));
+
+console.log(Math.random());
+console.log(Math.round(Math.random()*10));
